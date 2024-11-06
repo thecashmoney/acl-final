ef5b21b6be5cecb8c8fdfe4d3fae1438bc037e21
thecashmoney <elliottjang1@gmail.com>
2024-11-05 19:13:27 -0800

    finished hand (untested)

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimateHandOnInput : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
     void Start()
     {
         
@@ -11,6 +15,10 @@ public class AnimateHandOnInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
     }
 }
